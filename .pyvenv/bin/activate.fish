41f343d340c0cec93c2d2fd572609fe53fd2c88a
milavdabgar <milav.dabgar@gmail.com>
2024-01-06 10:53:39 +0530

    flask_sec_too login done

--- .pyvenv/bin/activate.fish
+++ .pyvenv/bin/activate.fish
@@ -33,7 +33,7 @@ end
 # Unset irrelevant variables.
 deactivate nondestructive
 
-set -gx VIRTUAL_ENV "/home/milav/code/gppPortal/.pyvenv"
+set -gx VIRTUAL_ENV "/home/milav/Codes/gppPortal/.pyvenv"
 
 set -gx _OLD_VIRTUAL_PATH $PATH
 set -gx PATH "$VIRTUAL_ENV/bin" $PATH
