35ea6df0fc6cc68c6d7d4a629eed543e05877d7a
milavdabgar <milav.dabgar@gmail.com>
2024-01-08 11:16:04 +0530

    added new flask extensions

--- app/email.py
+++ app/email.py
@@ -9,8 +9,9 @@ def send_async_email(app, msg):
         mail.send(msg)
 
 
-def send_email(subject, sender, recipients, text_body, html_body,
-               attachments=None, sync=False):
+def send_email(
+    subject, sender, recipients, text_body, html_body, attachments=None, sync=False
+):
     msg = Message(subject, sender=sender, recipients=recipients)
     msg.body = text_body
     msg.html = html_body
@@ -20,5 +21,6 @@ def send_email(subject, sender, recipients, text_body, html_body,
     if sync:
         mail.send(msg)
     else:
-        Thread(target=send_async_email,
-            args=(current_app._get_current_object(), msg)).start()
+        Thread(
+            target=send_async_email, args=(current_app._get_current_object(), msg)
+        ).start()
